fd69efa75e2290a9a306e4b43ae8c41d123d54d3
“George <gbaidoo94@gmail.com>
2022-10-07 03:29:42 +0000

    Added new meeting files

--- decision.py
+++ decision.py
@@ -10,3 +10,11 @@ elif(n < 5):
     print("Small number")
 elif(n ==5):
     print("equals 5")
+    
+numbers = [random.randint(0,10) for i in range(10)]
+
+print(numbers)
+
+if(threshold in numbers):
+    print(str(threshold) + " is in numbers")
+    
